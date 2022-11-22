d3ff688af4b374c62c654c785c11aad961bf5051
shiu <shiyu.wang@wengegroup.com>
2022-11-22 14:53:02 +0800

    fix: node env not work

--- src/TimeTask.ts
+++ src/TimeTask.ts
@@ -1,53 +1,64 @@
 type Func = (...args: unknown[]) => void;
+export interface TaskUnit {
+  context: any,
+  callback: (context: any) => void
+}
+const isObject = (target: any) => Object.prototype.toString.call(target) === '[object Object]'
 class TimeTask {
-	protected timeout = 1000;
-	protected tasks: Func[] = [];
-	protected running: boolean = false;
-	private timer: number | null = null;
-	constructor(timeout: number) {
-		this.timeout = timeout;
-	}
-	addTask(task: Func) {
-		this.tasks.push(task);
-		return this;
-	}
-	removeTask(task: Func) {
-		const index = this.tasks.indexOf(task);
-		if (index > -1) {
-			this.tasks.splice(index, 1);
-		}
-		return this;
-	}
-	clearTask() {
-		this.tasks.length = 0;
-	}
-	perform() {
-		this.tasks.slice().forEach((task) => task());
-		return this;
-	}
-	protected step() {
-		this.perform();
-		this.timer = window.setTimeout(() => {
-			this.step();
-		}, this.timeout);
-	}
-	start() {
-		if (this.running) return;
-		this.running = true;
-		this.step();
-		return this;
-	}
-	stop() {
-		if (!this.running) return;
-		this.running = false;
-		this.timer && window.clearTimeout(this.timer);
-		return this;
-	}
+  protected timeout = 1000;
+  protected tasks: (Func | TaskUnit)[] = [];
+  protected running: boolean = false;
+  private timer: number | null = null;
+  constructor(timeout: number) {
+    this.timeout = timeout;
+  }
+  addTask(task: Func | TaskUnit) {
+    this.tasks.push(task);
+    return this;
+  }
+  removeTask(task: Func) {
+    const index = this.tasks.indexOf(task);
+    if (index > -1) {
+      this.tasks.splice(index, 1);
+    }
+    return this;
+  }
+  clearTask() {
+    this.tasks.length = 0;
+  }
+  perform() {
+    this.tasks.slice().forEach((task) => {
+      if (typeof task === 'function') {
+        task()
+      } else if (isObject(task)) {
+        task.callback?.(task.context)
+      }
+    });
+    return this;
+  }
+  protected step() {
+    this.perform();
+    this.timer = setTimeout(() => {
+      this.step();
+    }, this.timeout);
+  }
+  start() {
+    if (this.running) return;
+    this.running = true;
+    this.step();
+    return this;
+  }
+  stop() {
+    if (!this.running) return;
+    this.running = false;
+    this.timer && window.clearTimeout(this.timer);
+    return this;
+  }
 
-	destroy() {
-		this.stop();
-		this.clearTask();
-	}
+  destroy() {
+    this.stop();
+    this.clearTask();
+  }
 }
 
 export default TimeTask
